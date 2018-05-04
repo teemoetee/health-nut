74937ac9f4ec499478cbbc7bae6f3bce59143540
Timothy <titus.timothy.n@gmail.com>
2018-05-03 23:20:53 -0400

    added math behind REE and TDEE

--- health-nut/src/App.js
+++ health-nut/src/App.js
@@ -2,12 +2,90 @@ import React, { Component } from 'react';
 import './App.css';
 import MacroCalc from './MacroCalc';
 
+const initial_state = {
+  gender: '',
+  weight: 0,
+  height: 0,
+  age: 0,
+  activity: '',
+  ree: 0,
+  tdee: 0,
+  protein: 0,
+  carbohydrates: 0,
+  fat: 0
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = initial_state;
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  calculateRee = () => {
+    var newRee = 0;
+    if(this.state.gender === 'male') {
+      newRee = Number(10 * this.state.weight + 6.25 * this.state.height - 5 * this.state.age + 5);
+    }else if(this.state.gender === 'female') {
+      newRee = Number(10 * this.state.weight + 6.25 * this.state.height - 5 * this.state.age - 161);
+    }
+    return newRee;
+  }
+  calculateTdee = () => {
+    var newTdee = 0;
+    if(this.state.gender === 'male') {
+      newTdee = (Number(10 * this.state.weight + 6.25 * this.state.height - 5 * this.state.age + 5) * this.state.activity);
+    }else if(this.state.gender === 'female') {
+      newTdee = (Number(10 * this.state.weight + 6.25 * this.state.height - 5 * this.state.age - 161) * this.state.activity);
+    }
+    return newTdee;
+  }
+
   render() {
+    const weight = this.state.weight;
+    const height = this.state.height;
+    const age = this.state.age;
+    const gender = this.state.gender;
+    const ree = this.state.ree;
+    const tdee = this.state.tdee;
+    const protein = this.state.protein;
+    const carbohydrates = this.state.carbohydrates;
+    const fat = this.state.fat;
+
     return (
-      <div className="App">
-      <h1>Health Nut</h1>
-      <MacroCalc />
+      <div id='master'>
+        <div id='input'>
+          <label>Gender: </label>
+          <select name='gender' value={this.state.gender} onChange={this.handleChange}>
+            <option value=''>Select Gender...</option>
+            <option value='male'>Male</option>
+            <option value='female'>Female</option>
+          </select><br />
+          <label>Weight(Kg): </label>
+          <input type='text' name='weight' onChange={this.handleChange} /><br />
+          <label>Height(Cm): </label>
+          <input type='text' name='height' onChange={this.handleChange} /><br />
+          <label>Age(y): </label>
+          <input type='text' name='age' onChange={this.handleChange} /><br />
+          <label>Activity: </label>
+          <select name='activity' value={this.state.activity} onChange={this.handleChange}>
+            <option value='default'>Select Activity...</option>
+            <option value='1.2'>Sedentary</option>
+            <option value='1.375'>Lightly Active</option>
+            <option value='1.55'>Moderately Active</option>
+            <option value='1.725'>Very Active</option>
+          </select><br /><br />
+        </div>
+        <MacroCalc
+          gender={gender}
+          ree={this.calculateRee()}
+          tdee={this.calculateTdee()}
+          protein={protein}
+          carbohydrates={carbohydrates}
+          fat={fat}
+        />
       </div>
     );
   }
--- health-nut/src/MacroCalc.js
+++ health-nut/src/MacroCalc.js
@@ -1,57 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-const initial_state = {
-    gender: '',
-    weight: 0,
-    height: 0,
-    age: 0,
-    activity: '',
-    ree: 0,
-    tdee: 0,
-    protein: 0,
-    carbohydrates: 0,
-    fat: 0
-}
 
 class MacroCalc extends Component {
-    constructor() {
-        super();
-        this.state = initial_state;
-    }
-    calculateMacros = () => {
-        //calculate macros here :)
-    }
     render() {
+        var gender = this.props.gender;
         return (
-            <div id='master'>
-                <div id='input'>
-                    <label>Gender: </label>
-                    <input type='radio' name='gender' value='male' /><label>Male</label><input type='radio' name='gender' value='female' /><label>Female</label><br />
-                    <label>Weight(Kg): </label>
-                    <input type='text' /><br />
-                    <label>Height(Cm): </label>
-                    <input type='text' /><br />
-                    <label>Age(y): </label>
-                    <input type='text' /><br />
-                    <label>Activity: </label>
-                    <select>
-                        <option>Sedentary</option>
-                        <option>Lightly Active</option>
-                        <option>Moderately Active</option>
-                        <option>Very Active</option>
-                    </select><br /><br />
-                </div>
-                <input type='button' value='Submit' onClick={this.calculateMacros}/>
-                <div id='output'>
-                    <label>REE: </label><br />
-                    <label>TDEE: </label><br />
-                    <label>Protein: </label><br />
-                    <label>Carbohydrates: </label><br />
-                    <label>Fat: </label><br />
-                    
-                </div>
-
+            <div id='output'>
+                <div>Gender: {gender}</div><br />
+                <div>REE:  {this.props.ree}</div><br />
+                <div>TDEE: {this.props.tdee}</div><br />
+                <div>Protein: {this.props.protein}</div><br />
+                <div>Carbohydrates: {this.props.carbohydrates}</div><br />
+                <div>Fat: {this.props.fat}</div><br />
             </div>
-        )
+        );
     }
+
+
 } export default MacroCalc;
